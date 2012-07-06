0fficialcrewcpmr34/-ExyBomb_Crew

commit 856fe52f3023ea3410a52ac30757416161598eda

ARM: shmobile: fix smp build
I got build errors with the new version now because machine_is_kzm9g is no longer
defined:

arch/arm/mach-shmobile/platsmp.c: In function 'shmobile_smp_get_core_count':
arch/arm/mach-shmobile/platsmp.c:29:2: error: implicit declaration of function 'of_machine_is_compatible'

Replace the missing function with a call to of_machine_is_compatible.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Acked-by: "Rafael J. Wysocki" <rjw@sisk.pl>
Acked-by: Magnus Damm <magnus.damm@gmail.com>

arch/arm/mach-shmobile/platsmp.c
@@ -16,11 +16,13 @@
 #include <linux/device.h>
 #include <linux/smp.h>
 #include <linux/io.h>
+#include <linux/of.h>
 #include <asm/hardware/gic.h>
 #include <asm/mach-types.h>
 #include <mach/common.h>
 
-#define is_sh73a0() (machine_is_ag5evm() || machine_is_kota2() || machine_is_kzm9g())
+#define is_sh73a0() (machine_is_ag5evm() || machine_is_kota2() || \
+			of_machine_is_compatible("renesas,sh73a0"))
 #define is_r8a7779() machine_is_marzen()
 
 static unsigned int __init shmobile_smp_get_core_count(void)

arch/arm/boot/dts/sh73a0-kzm9g.dts
@@ -13,7 +13,7 @@
 
 / {
 	model = "KZM-A9-GT";
-	compatible = "renesas,kzm9g";
+	compatible = "renesas,kzm9g", "renesas,sh73a0";
 
 	memory {
 		device_type = "memory";

commit 3f1ab3f3dcfc6fb3304088e3f82d4595e368b818

ARM: shmobile: fix platsmp.c build when ARCH_SH73A0=n
Fix build error in the case of SMP=y but ARCH_SH73A0=n
introduced by:

856fe52 ARM: shmobile: fix smp build

The use of of_machine_is_compatible() will link in the
the SoC-specific symbols:
"sh73a0_get_core_count", "sh73a0_smp_prepare_cpus",
"sh73a0_secondary_init" and "sh73a0_boot_secondary".

This patch adds an ugly #ifdef wrapper as a stop-gap
solution.

Signed-off-by: Magnus Damm <damm@opensource.se>
Tested-by: Tested-by: Simon Horman <horms@verge.net.au>
Acked-by: "Rafael J. Wysocki" <rjw@sisk.pl>
Signed-off-by: Arnd Bergmann <arnd@arndb.de>

arch/arm/mach-shmobile/platsmp.c
@@ -22,8 +22,13 @@
 #include <mach/common.h>
 #include <mach/emev2.h>
 
+#ifdef CONFIG_ARCH_SH73A0
 #define is_sh73a0() (machine_is_ag5evm() || machine_is_kota2() || \
 			of_machine_is_compatible("renesas,sh73a0"))
+#else
+#define is_sh73a0() (0)
+#endif
+
 #define is_r8a7779() machine_is_marzen()
 
 #ifdef CONFIG_ARCH_EMEV2