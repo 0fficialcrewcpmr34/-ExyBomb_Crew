0fficialcrewcpmr34/-ExyBomb_Crew

commit 59dbb02604216d7eb8f3ee06c43757de91bbc685

[media] tlg2300: fix missing check for audio creation
If we fail to set up the capture device we go through negative indexes and
badness happens. Add the missing test.
Resolves-bug: https://bugzilla.kernel.org/show_bug.cgi?id=44551

Signed-off-by: Alan Cox <alan@linux.intel.com>
Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

drivers/media/usb/tlg2300/pd-alsa.c
@@ -305,6 +305,10 @@ int poseidon_audio_init(struct poseidon *p)
 		return ret;
 
 	ret = snd_pcm_new(card, "poseidon audio", 0, 0, 1, &pcm);
+	if (ret < 0) {
+		snd_free_card(card);
+		return ret;
+	}
 	snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE, &pcm_capture_ops);
 	pcm->info_flags   = 0;
 	pcm->private_data = p;

commit 540e63b67b9bc1a8c48152ec280a08d69cc83152

[media] pd-alsa: fix compilation breakage by commit 59dbb0260
commit 59dbb0260 broke compilation, as it reverted the name of the
usb card free function.

Cc: Alan Cox <alan@linux.intel.com>
Signed-off-by: Mauro Carvalho Chehab <mchehab@redhat.com>

drivers/media/usb/tlg2300/pd-alsa.c
@@ -306,7 +306,7 @@ int poseidon_audio_init(struct poseidon *p)
 
 	ret = snd_pcm_new(card, "poseidon audio", 0, 0, 1, &pcm);
 	if (ret < 0) {
-		snd_free_card(card);
+		snd_card_free(card);
 		return ret;
 	}
 	snd_pcm_set_ops(pcm, SNDRV_PCM_STREAM_CAPTURE, &pcm_capture_ops);