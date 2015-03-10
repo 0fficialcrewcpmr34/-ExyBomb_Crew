0fficialcrewcpmr34/-ExyBomb_Crew

commit 38f9cd9912fc7075a6ef4cd05736c8e89a83fee2

goldfish: remove unreachable line of code
Signed-off-by: Alan Cox <alan@linux.intel.com>
Signed-off-by: Jiri Kosina <jkosina@suse.cz>

drivers/tty/goldfish.c
@@ -293,7 +293,6 @@ static int goldfish_tty_probe(struct platform_device *pdev)
 	mutex_unlock(&goldfish_tty_lock);
 	return 0;
 
-	tty_unregister_device(goldfish_tty_driver, i);
 err_tty_register_device_failed:
 	free_irq(irq, pdev);
 err_request_irq_failed:

commit 9e9525baac768b5734b8d2594f7d0c97013a0980

goldfish: goldfish_tty_probe() is not using 'i' any more
The only place where 'i' has been used was a dead code that
got removed by 38f9cd9912fc7 ("goldfish: remove unreachable line
of code"). Remove the last reference to the variable as well.

Fixes: 38f9cd9912fc7 ("goldfish: remove unreachable line of code")
Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
Signed-off-by: Jiri Kosina <jkosina@suse.cz>

drivers/tty/goldfish.c
@@ -229,7 +229,6 @@ static int goldfish_tty_probe(struct platform_device *pdev)
 {
 	struct goldfish_tty *qtty;
 	int ret = -EINVAL;
-	int i;
 	struct resource *r;
 	struct device *ttydev;
 	void __iomem *base;