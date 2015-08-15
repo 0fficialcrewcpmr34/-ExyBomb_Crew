0fficialcrewcpmr34/-ExyBomb_Crew

commit 59a084e135dd174dda72b9c096e3ddb82b97547a

staging: comedi: me4000: remove 'board' from me4000_ai_insn_read()
The 'board' pointer is only used in this function to verify that the
'chan' is valid for an aref of AREF_DIFF. For differential inputs, the
maximum channel is half the subdevice 'n_chan'. Use that instead and
remove the 'board' variable.

Also, the comedi core does not validate the aref flags. Add a check
to ensure that the subdevice actually supports the AREF_DIFF mode.

Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/comedi/drivers/me4000.c
@@ -436,10 +436,10 @@ static void me4000_reset(struct comedi_device *dev)
   ===========================================================================*/
 
 static int me4000_ai_insn_read(struct comedi_device *dev,
-			       struct comedi_subdevice *subdevice,
-			       struct comedi_insn *insn, unsigned int *data)
+			       struct comedi_subdevice *s,
+			       struct comedi_insn *insn,
+			       unsigned int *data)
 {
-	const struct me4000_board *board = dev->board_ptr;
 	int chan = CR_CHAN(insn->chanspec);
 	int rang = CR_RANGE(insn->chanspec);
 	int aref = CR_AREF(insn->chanspec);
@@ -481,13 +481,19 @@ static int me4000_ai_insn_read(struct comedi_device *dev,
 		break;
 
 	case AREF_DIFF:
+		if (!(s->subdev_flags && SDF_DIFF)) {
+			dev_err(dev->class_dev,
+				"Differential inputs are not available\n");
+			return -EINVAL;
+		}
+
 		if (rang == 0 || rang == 1) {
 			dev_err(dev->class_dev,
 				"Range must be bipolar when aref = diff\n");
 			return -EINVAL;
 		}
 
-		if (chan >= board->ai_diff_nchan) {
+		if (chan >= (s->n_chan / 2)) {
 			dev_err(dev->class_dev,
 				"Analog input is not available\n");
 			return -EINVAL;

commit d52c85b44cf61819a1d29240db4c2bc04b99f97d

staging: comedi: me4000: use bitwise AND instead of logical
This was supposed to bitwise AND but there is a typo.

Fixes: 59a084e135dd ('staging: comedi: me4000: remove 'board' from me4000_ai_insn_read()')
Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
Reviewed-by: H Hartley Sweeten <hsweeten@visionengravers.com>
Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/comedi/drivers/me4000.c
@@ -480,7 +480,7 @@ static int me4000_ai_insn_read(struct comedi_device *dev,
 
 	entry = chan | ME4000_AI_LIST_RANGE(range);
 	if (aref == AREF_DIFF) {
-		if (!(s->subdev_flags && SDF_DIFF)) {
+		if (!(s->subdev_flags & SDF_DIFF)) {
 			dev_err(dev->class_dev,
 				"Differential inputs are not available\n");
 			return -EINVAL;
@@ -559,7 +559,7 @@ static int me4000_ai_check_chanlist(struct comedi_device *dev,
 		}
 
 		if (aref == AREF_DIFF) {
-			if (!(s->subdev_flags && SDF_DIFF)) {
+			if (!(s->subdev_flags & SDF_DIFF)) {
 				dev_err(dev->class_dev,
 					"Differential inputs are not available\n");
 				return -EINVAL;