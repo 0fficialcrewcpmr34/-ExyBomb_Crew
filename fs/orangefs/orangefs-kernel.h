0fficialcrewcpmr34/-ExyBomb_Crew

commit 9ec681edd3bc963f03fd25839f8f216b01c425c7

Orangefs: change pvfs2 filenames to orangefs
Also changed references within source files that referred to
header files whose names had changed.

Signed-off-by: Mike Marshall <hubcap@omnibond.com>

fs/orangefs/orangefs-kernel.h
@@ -53,7 +53,7 @@
 
 #include <asm/unaligned.h>
 
-#include "pvfs2-dev-proto.h"
+#include "orangefs-dev-proto.h"
 
 #ifdef ORANGEFS_KERNEL_DEBUG
 #define ORANGEFS_DEFAULT_OP_TIMEOUT_SECS       10

Makefile
@@ -1,7 +1,7 @@
 VERSION = 4
 PATCHLEVEL = 4
 SUBLEVEL = 0
-EXTRAVERSION = -rc1
+EXTRAVERSION = -rc1-o
 NAME = Blurry Fish Butt
 
 # *DOCUMENTATION*

fs/orangefs/Makefile
@@ -4,7 +4,7 @@
 
 obj-$(CONFIG_ORANGEFS_FS) += orangefs.o
 
-orangefs-objs := acl.o file.o pvfs2-cache.o pvfs2-utils.o xattr.o dcache.o \
-		 inode.o pvfs2-sysfs.o pvfs2-mod.o super.o devpvfs2-req.o \
-		 namei.o symlink.o dir.o pvfs2-bufmap.o \
-		 pvfs2-debugfs.o waitqueue.o
+orangefs-objs := acl.o file.o orangefs-cache.o orangefs-utils.o xattr.o \
+		 dcache.o inode.o orangefs-sysfs.o orangefs-mod.o super.o \
+		 devorangefs-req.o namei.o symlink.o dir.o orangefs-bufmap.o \
+		 orangefs-debugfs.o waitqueue.o

fs/orangefs/acl.c
@@ -5,8 +5,8 @@
  */
 
 #include "protocol.h"
-#include "pvfs2-kernel.h"
-#include "pvfs2-bufmap.h"
+#include "orangefs-kernel.h"
+#include "orangefs-bufmap.h"
 #include <linux/posix_acl_xattr.h>
 #include <linux/fs_struct.h>
 

fs/orangefs/dcache.c
@@ -9,7 +9,7 @@
  */
 
 #include "protocol.h"
-#include "pvfs2-kernel.h"
+#include "orangefs-kernel.h"
 
 /* Returns 1 if dentry can still be trusted, else 0. */
 static int orangefs_revalidate_lookup(struct dentry *dentry)

fs/orangefs/devorangefs-req.c
@@ -8,9 +8,9 @@
  */
 
 #include "protocol.h"
-#include "pvfs2-kernel.h"
-#include "pvfs2-dev-proto.h"
-#include "pvfs2-bufmap.h"
+#include "orangefs-kernel.h"
+#include "orangefs-dev-proto.h"
+#include "orangefs-bufmap.h"
 
 #include <linux/debugfs.h>
 #include <linux/slab.h>

fs/orangefs/dir.c
@@ -5,8 +5,8 @@
  */
 
 #include "protocol.h"
-#include "pvfs2-kernel.h"
-#include "pvfs2-bufmap.h"
+#include "orangefs-kernel.h"
+#include "orangefs-bufmap.h"
 
 struct readdir_handle_s {
 	int buffer_index;

fs/orangefs/file.c
@@ -9,8 +9,8 @@
  */
 
 #include "protocol.h"
-#include "pvfs2-kernel.h"
-#include "pvfs2-bufmap.h"
+#include "orangefs-kernel.h"
+#include "orangefs-bufmap.h"
 #include <linux/fs.h>
 #include <linux/pagemap.h>
 
@@ -186,7 +186,7 @@ populate_shared_memory:
 	}
 
 	if (ret < 0) {
-		handle_io_error(); /* defined in pvfs2-kernel.h */
+		handle_io_error();
 		/*
 		 * don't write an error to syslog on signaled operation
 		 * termination unless we've got debugging turned on, as

commit fe01a19d653d1ac943e7c45a321131062ae9c0fb

orangefs: fix typo in ornagefs_inode_lock
Orangefs fails to build on 32-bit SMP configurations due to a simple
misspelling, this does the obvious fix.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Fixes: 9ec681edd3bc ("Orangefs: change pvfs2 filenames to orangefs")
Signed-off-by: Mike Marshall <hubcap@omnibond.com>

fs/orangefs/orangefs-kernel.h
@@ -770,7 +770,7 @@ do {									\
 static inline void orangefs_i_size_write(struct inode *inode, loff_t i_size)
 {
 #if BITS_PER_LONG == 32 && defined(CONFIG_SMP)
-	ornagefs_inode_lock(inode);
+	orangefs_inode_lock(inode);
 #endif
 	i_size_write(inode, i_size);
 #if BITS_PER_LONG == 32 && defined(CONFIG_SMP)