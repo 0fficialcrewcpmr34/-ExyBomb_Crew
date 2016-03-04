0fficialcrewcpmr34/-ExyBomb_Crew

commit 8d9ab9ffd0a6a9d1d7a24ee1445053fb5b5306a1

v4l2-mc.h: Add stubs for the V4L2 PM/pipeline routines
Let's add stubs for the case where the Kernel gets compiled
without MEDIA_CONTROLLER.

Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

include/media/v4l2-mc.h
@@ -207,7 +207,7 @@ int v4l2_pipeline_pm_use(struct media_entity *entity, int use);
 int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
 			      unsigned int notification);
 
-#else
+#else /* CONFIG_MEDIA_CONTROLLER */
 
 static inline int v4l2_mc_create_media_graph(struct media_device *mdev)
 {
@@ -228,5 +228,17 @@ static int v4l_vb2q_enable_media_source(struct vb2_queue *q)
 {
 	return 0;
 }
-#endif
+
+int v4l2_pipeline_pm_use(struct media_entity *entity, int use);
+{
+	return 0;
+}
+
+int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
+			      unsigned int notification);
+{
+	return 0;
+}
+
+#endif /* CONFIG_MEDIA_CONTROLLER */
 #endif /* _V4L2_MC_H */

commit 97dd9067ba259e67b4a102d8dd1e7ee25c8bdc84

[media] v4l2-mc.h: fix yet more compiler errors
The newly added functions have an extra semicolon, which
prevents compilation, and they need to be marked inline:

In file included from ../include/media/tuner.h:23:0,
                 from ../drivers/media/tuners/tuner-simple.c:10:
../include/media/v4l2-mc.h:233:1: error: expected identifier or '(' before '{' token

Remove spurious return, remove copy-and-pasted semi-colons, add static
inline.

Fixes: 8d9ab9ffd0a6 ("v4l2-mc.h: Add stubs for the V4L2 PM/pipeline routines")

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Signed-off-by: Hans Verkuil <hans.verkuil@cisco.com>
Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

include/media/v4l2-mc.h
@@ -221,7 +221,6 @@ static inline int v4l_enable_media_source(struct video_device *vdev)
 
 static inline void v4l_disable_media_source(struct video_device *vdev)
 {
-	return;
 }
 
 static inline int v4l_vb2q_enable_media_source(struct vb2_queue *q)
@@ -229,13 +228,13 @@ static inline int v4l_vb2q_enable_media_source(struct vb2_queue *q)
 	return 0;
 }
 
-int v4l2_pipeline_pm_use(struct media_entity *entity, int use);
+static inline int v4l2_pipeline_pm_use(struct media_entity *entity, int use)
 {
 	return 0;
 }
 
-int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
-			      unsigned int notification);
+static inline int v4l2_pipeline_link_notify(struct media_link *link, u32 flags,
+					    unsigned int notification)
 {
 	return 0;
 }