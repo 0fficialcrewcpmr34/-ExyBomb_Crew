0fficialcrewcpmr34/-ExyBomb_Crew

commit e81fb619807235c951ec18f75b3cf193f1163703

staging: comedi: ni_tio: make ni_tio_winsn() a proper comedi (*insn_write)
Change the parameters to ni_tio_winsn() to make it a proper comedi
(*insn_write) function. This allows using it directly and removing the
wrapper functions in the ni_660x and ni_mio_common modules.

For aesthetics, rename the function.

Signed-off-by: H Hartley Sweeten <hsweeten@visionengravers.com>
Reviewed-by: Ian Abbott <abbotti@mev.co.uk>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/comedi/drivers/ni_mio_common.c
@@ -276,9 +276,6 @@ static int ni_ao_reset(struct comedi_device *dev, struct comedi_subdevice *s);
 
 static int ni_8255_callback(int dir, int port, int data, unsigned long arg);
 
-static int ni_gpct_insn_write(struct comedi_device *dev,
-			      struct comedi_subdevice *s,
-			      struct comedi_insn *insn, unsigned int *data);
 static int ni_gpct_insn_config(struct comedi_device *dev,
 			       struct comedi_subdevice *s,
 			       struct comedi_insn *insn, unsigned int *data);
@@ -4443,7 +4440,7 @@ static int ni_E_init(struct comedi_device *dev)
 		else
 			s->maxdata = 0xffffff;
 		s->insn_read = ni_tio_insn_read;
-		s->insn_write = &ni_gpct_insn_write;
+		s->insn_write = ni_tio_insn_read;
 		s->insn_config = &ni_gpct_insn_config;
 #ifdef PCIDMA
 		s->subdev_flags |= SDF_CMD_READ /* | SDF_CMD_WRITE */;
@@ -5018,14 +5015,6 @@ static int ni_gpct_insn_config(struct comedi_device *dev,
 {
 	struct ni_gpct *counter = s->private;
 	return ni_tio_insn_config(counter, insn, data);
-}
-
-static int ni_gpct_insn_write(struct comedi_device *dev,
-			      struct comedi_subdevice *s,
-			      struct comedi_insn *insn, unsigned int *data)
-{
-	struct ni_gpct *counter = s->private;
-	return ni_tio_winsn(counter, insn, data);
 }
 
 #ifdef PCIDMA

drivers/staging/comedi/drivers/ni_660x.c
@@ -1002,15 +1002,6 @@ ni_660x_GPCT_insn_config(struct comedi_device *dev, struct comedi_subdevice *s,
 	return ni_tio_insn_config(counter, insn, data);
 }
 
-static int ni_660x_GPCT_winsn(struct comedi_device *dev,
-			      struct comedi_subdevice *s,
-			      struct comedi_insn *insn, unsigned int *data)
-{
-	struct ni_gpct *counter = s->private;
-
-	return ni_tio_winsn(counter, insn, data);
-}
-
 static int ni_660x_dio_insn_bits(struct comedi_device *dev,
 				 struct comedi_subdevice *s,
 				 struct comedi_insn *insn, unsigned int *data)
@@ -1233,7 +1224,7 @@ static int ni_660x_auto_attach(struct comedi_device *dev,
 			s->n_chan = 3;
 			s->maxdata = 0xffffffff;
 			s->insn_read = ni_tio_insn_read;
-			s->insn_write = ni_660x_GPCT_winsn;
+			s->insn_write = ni_tio_insn_write;
 			s->insn_config = ni_660x_GPCT_insn_config;
 			s->do_cmd = &ni_660x_cmd;
 			s->len_chanlist = 1;

drivers/staging/comedi/drivers/ni_tio.c
@@ -1638,9 +1638,12 @@ static unsigned ni_tio_next_load_register(struct ni_gpct *counter)
 		return NITIO_LOADA_REG(cidx);
 }
 
-int ni_tio_winsn(struct ni_gpct *counter, struct comedi_insn *insn,
-		 unsigned int *data)
+int ni_tio_insn_write(struct comedi_device *dev,
+		      struct comedi_subdevice *s,
+		      struct comedi_insn *insn,
+		      unsigned int *data)
 {
+	struct ni_gpct *counter = s->private;
 	struct ni_gpct_device *counter_dev = counter->counter_dev;
 	const unsigned channel = CR_CHAN(insn->chanspec);
 	unsigned cidx = counter->counter_index;
@@ -1673,7 +1676,7 @@ int ni_tio_winsn(struct ni_gpct *counter, struct comedi_insn *insn,
 	}
 	return 0;
 }
-EXPORT_SYMBOL_GPL(ni_tio_winsn);
+EXPORT_SYMBOL_GPL(ni_tio_insn_write);
 
 static int __init ni_tio_init_module(void)
 {

drivers/staging/comedi/drivers/ni_tio.h
@@ -141,7 +141,8 @@ int ni_tio_insn_read(struct comedi_device *, struct comedi_subdevice *,
 		     struct comedi_insn *, unsigned int *data);
 int ni_tio_insn_config(struct ni_gpct *, struct comedi_insn *,
 		       unsigned int *data);
-int ni_tio_winsn(struct ni_gpct *, struct comedi_insn *, unsigned int *data);
+int ni_tio_insn_write(struct comedi_device *, struct comedi_subdevice *,
+		      struct comedi_insn *, unsigned int *data);
 int ni_tio_cmd(struct ni_gpct *, struct comedi_async *);
 int ni_tio_cmdtest(struct ni_gpct *, struct comedi_cmd *);
 int ni_tio_cancel(struct ni_gpct *);

commit a70a85851ebde03821542416c97098c6d5228a4e

staging: comedi: ni_mio_common: fix wrong insn_write handler
For counter subdevices, the `s->insn_write` handler is being set to the
wrong function, `ni_tio_insn_read()`.  It should be
`ni_tio_insn_write()`.

Signed-off-by: Ian Abbott <abbotti@mev.co.uk>
Reported-by: Éric Piel <piel@delmic.com>
Fixes: e81fb6198072 ("staging: comedi: ni_tio: make ni_tio_winsn() a
  proper comedi (*insn_write)"
Cc: <stable@vger.kernel.org> # 3.17+
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/comedi/drivers/ni_mio_common.c
@@ -5480,7 +5480,7 @@ static int ni_E_init(struct comedi_device *dev,
 		s->maxdata	= (devpriv->is_m_series) ? 0xffffffff
 							 : 0x00ffffff;
 		s->insn_read	= ni_tio_insn_read;
-		s->insn_write	= ni_tio_insn_read;
+		s->insn_write	= ni_tio_insn_write;
 		s->insn_config	= ni_tio_insn_config;
 #ifdef PCIDMA
 		if (dev->irq && devpriv->mite) {