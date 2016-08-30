0fficialcrewcpmr34/-ExyBomb_Crew

commit 4afe3a81e035b3b24e957a0c64186bf2fa8b3778

video: fbdev: mb862xx: mb862xx-i2c: don't print error when adding adapter fails
Don't print error when adding adapter fails. The core will do this for
us now.

Signed-off-by: Wolfram Sang <wsa-dev@sang-engineering.com>
[tomi.valkeinen@ti.com: fixed the description]
Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

drivers/video/fbdev/mb862xx/mb862xx-i2c.c
@@ -162,12 +162,7 @@ int mb862xx_i2c_init(struct mb862xxfb_par *par)
 	mb862xx_i2c_adapter.algo_data = par;
 	par->adap = &mb862xx_i2c_adapter;
 
-	ret = i2c_add_adapter(par->adap);
-	if (ret < 0) {
-		dev_err(par->dev, "failed to add %s\n",
-			mb862xx_i2c_adapter.name);
-	}
-	return ret;
+	return i2c_add_adapter(par->adap);
 }
 
 void mb862xx_i2c_exit(struct mb862xxfb_par *par)

commit e7313b86169e2548cfa23d33f2958863a91234c7

video: fbdev: mb862xx: remove unused variable
A cleanup patch that removed some code left behind an unused
variable:

drivers/video/fbdev/mb862xx/mb862xx-i2c.c: In function 'mb862xx_i2c_init':
drivers/video/fbdev/mb862xx/mb862xx-i2c.c:160:6: error: unused variable 'ret' [-Werror=unused-variable]

This removes that variable as well.

Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Fixes: 4afe3a81e035 ("video: fbdev: mb862xx: mb862xx-i2c: don't print error when adding adapter fails")
Acked-by: Wolfram Sang <wsa@the-dreams.de>
Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

drivers/video/fbdev/mb862xx/mb862xx-i2c.c
@@ -157,8 +157,6 @@ static struct i2c_adapter mb862xx_i2c_adapter = {
 
 int mb862xx_i2c_init(struct mb862xxfb_par *par)
 {
-	int ret;
-
 	mb862xx_i2c_adapter.algo_data = par;
 	par->adap = &mb862xx_i2c_adapter;
 