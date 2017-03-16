0fficialcrewcpmr34/-ExyBomb_Crew

commit 017b7f62fd8d47ded12e675ac7f6c31dbf2cb5e5

staging: ks7010: move comparison to right hand side
Checkpatch emits WARNING: Comparisons should place the constant on the
right side of the test.

Move constant to right hand side of test, modify operator to ensure
logic is maintained.

Signed-off-by: Tobin C. Harding <me@tobin.cc>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/ks7010/ks_wlan_net.c
@@ -2044,7 +2044,7 @@ static int ks_wlan_set_pmksa(struct net_device *dev,
 				}
 			}
 			if (ptr == &priv->pmklist.head) {	/* not find address. */
-				if (PMK_LIST_MAX > priv->pmklist.size) {	/* new cache data */
+				if (priv->pmklist.size <= PMK_LIST_MAX) {	/* new cache data */
 					for (i = 0; i < PMK_LIST_MAX; i++) {
 						pmk = &priv->pmklist.pmk[i];
 						if (!memcmp

commit 7a7a359a0758e9a37ca8e61f24243c3822c9550b

staging: ks7010: fix off by one error
Bug introduced in commit 017b7f6 by Tobin C. Harding.

Remove equals sign from comparison, fixing off by one error.

Fixes: 017b7f62fd8d ("staging: ks7010: move comparison to right hand side")
Reported-by: Dan Carpenter <dan.carpenter@oracle.com>
Signed-off-by: Tobin C. Harding <me@tobin.cc>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/staging/ks7010/ks_wlan_net.c
@@ -2044,7 +2044,7 @@ static int ks_wlan_set_pmksa(struct net_device *dev,
 		if (ptr != &priv->pmklist.head)	/* not find address. */
 			break;	/* case */
 
-		if (priv->pmklist.size <= PMK_LIST_MAX) {	/* new cache data */
+		if (priv->pmklist.size < PMK_LIST_MAX) {	/* new cache data */
 			for (i = 0; i < PMK_LIST_MAX; i++) {
 				pmk = &priv->pmklist.pmk[i];
 				if (!memcmp("\x00\x00\x00\x00\x00\x00",