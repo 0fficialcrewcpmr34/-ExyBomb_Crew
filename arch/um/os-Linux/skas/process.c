0fficialcrewcpmr34/-ExyBomb_Crew

commit 5b0e0530b89ea2bf05d1837264346eee378bfa6b

um: Set number of CPUs
Define NR_CPUS required by the timer subsystem.

Fixes this make warning:

    scripts/kconfig/conf  --oldconfig arch/x86/um/Kconfig
    kernel/time/Kconfig:155:warning: range is invalid

Signed-off-by: Nikola Kotur <kotnick@gmail.com>
Signed-off-by: Richard Weinberger <richard@nod.at>

arch/um/os-Linux/skas/process.c
@@ -233,9 +233,6 @@ static int userspace_tramp(void *stack)
 	return 0;
 }
 
-/* Each element set once, and only accessed by a single processor anyway */
-#undef NR_CPUS
-#define NR_CPUS 1
 int userspace_pid[NR_CPUS];
 
 int start_userspace(unsigned long stub_stack)

arch/um/Kconfig.common
@@ -62,3 +62,8 @@ config HZ
 config SUBARCH
 	string
 	option env="SUBARCH"
+
+config NR_CPUS
+	int
+	range 1 1
+	default 1

commit 23cf1bddcf7ead7b553fc2519384bb5e68fe7593

um: Add missing NR_CPUS include
We need linux/threads.h for that variable.

Fixes: 5b0e0530b89ea2 ("um: Set number of CPUs")
Signed-off-by: Richard Weinberger <richard@nod.at>

arch/um/os-Linux/skas/process.c
@@ -21,6 +21,7 @@
 #include <registers.h>
 #include <skas.h>
 #include <sysdep/stub.h>
+#include <linux/threads.h>
 
 int is_skas_winch(int pid, int fd, void *data)
 {