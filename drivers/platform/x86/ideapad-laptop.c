0fficialcrewcpmr34/-ExyBomb_Crew

commit ad7728e67f25183aa71c0663600234f77f0b8e46

platform/x86: ideapad-laptop: Switch touchpad attribute to be RO
For now let's restrict touchpad attribute to be read only.
We might revisit this in the future though.

Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

drivers/platform/x86/ideapad-laptop.c
@@ -453,7 +453,8 @@ static ssize_t touchpad_store(struct device *dev,
 	return count;
 }
 
-static DEVICE_ATTR_RW(touchpad);
+/* Switch to RO for now: It might be revisited in the future */
+static DEVICE_ATTR_RO(touchpad);
 
 static struct attribute *ideapad_attributes[] = {
 	&dev_attr_camera_power.attr,

commit 3acff8a7658d61f1241d30c5961e40358ad18a68

platform/x86: ideapad-laptop: hide unused 'touchpad_store'
A readonly sysfs property must not have a 'store' function:

drivers/platform/x86/ideapad-laptop.c:438:16: error: 'touchpad_store' defined but not used [-Werror=unused-function]

We can either comment it out or remove the function entirely,
without a good reason one or or another I picked the second option.

Fixes: ad7728e67f25 ("platform/x86: ideapad-laptop: Switch touchpad attribute to be RO")
Signed-off-by: Arnd Bergmann <arnd@arndb.de>
Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>

drivers/platform/x86/ideapad-laptop.c
@@ -435,9 +435,10 @@ static ssize_t touchpad_show(struct device *dev,
 	return sprintf(buf, "%lu\n", result);
 }
 
-static ssize_t touchpad_store(struct device *dev,
-			      struct device_attribute *attr,
-			      const char *buf, size_t count)
+/* Switch to RO for now: It might be revisited in the future */
+static ssize_t __maybe_unused touchpad_store(struct device *dev,
+					     struct device_attribute *attr,
+					     const char *buf, size_t count)
 {
 	struct ideapad_private *priv = dev_get_drvdata(dev);
 	bool state;
@@ -453,7 +454,6 @@ static ssize_t touchpad_store(struct device *dev,
 	return count;
 }
 
-/* Switch to RO for now: It might be revisited in the future */
 static DEVICE_ATTR_RO(touchpad);
 
 static struct attribute *ideapad_attributes[] = {